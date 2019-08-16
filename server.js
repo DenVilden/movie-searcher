8b042c337d5e070ea565d0da10ace5a1ff9806c3
Denis Tolmachev <oovilden@gmail.com>
2019-08-16 12:58:47 +0500

    remove min-width for mobile

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(compression());
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'build')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
--- src/routes/App.styles.js
+++ src/routes/App.styles.js
@@ -32,7 +32,6 @@ export default createGlobalStyle`
   body {
     background-color: #f8f8ff;
     margin: 0;
-    min-width: 320px;
     -moz-osx-font-smoothing: grayscale;
     -webkit-font-smoothing: antialiased;
   }
