b5fb27e400e53226cbbd86b3a7bb731877d841e4
Denis Tolmachev <oovilden@gmail.com>
2019-08-16 08:25:15 +0500

    fonts optimization

--- src/routes/App.styles.js
+++ src/routes/App.styles.js
@@ -1,6 +1,34 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+  /* latin */
+  @font-face {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 300;
+    src: local('Roboto Light'), local('Roboto-Light'),
+      url('https://fonts.gstatic.com/s/roboto/v20/KFOlCnqEu92Fr1MmSU5fBBc4AMP6lQ.woff2')
+        format('woff2');
+  }
+  /* latin */
+  @font-face {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Roboto'), local('Roboto-Regular'),
+      url('https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2')
+        format('woff2');
+  }
+  /* latin */
+  @font-face {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 500;
+    src: local('Roboto Medium'), local('Roboto-Medium'),
+      url('https://fonts.gstatic.com/s/roboto/v20/KFOlCnqEu92Fr1MmEU9fBBc4AMP6lQ.woff2')
+        format('woff2');
+  }
+
   body {
     background-color: #f8f8ff;
     margin: 0;
