ae3e69123178ac41679dd7ad01e08a1c5630a5f1
Denis Tolmachev <oovilden@gmail.com>
2020-03-13 12:41:26 +0500

    deploy

--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const withOffline = require('next-offline');
 
 const nextConfig = {
-  distDir: 'build',
   env: {
     REACT_APP_SERVER_URL: process.env.REACT_APP_SERVER_URL,
   },
