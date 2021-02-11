64e2ff2cb0dcccf139f6d9404c9bd4bc84d2fe4a
Denis Tolmachev <dentolmachevfe@gmail.com>
2021-02-11 17:48:04 +0500

    add husky and lint-staged

--- utils/styled-components.d.ts
+++ utils/styled-components.d.ts
@@ -1,4 +1,4 @@
-import type { theme as CustomTheme } from './utils/theme';
+import type { theme as CustomTheme } from './theme';
 
 declare module 'styled-components' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
